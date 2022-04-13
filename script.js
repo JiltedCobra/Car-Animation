a6cd659ca566371c6e7e8f3443bcd44bbf4b599c
JiltedCobra <99410623+JiltedCobra@users.noreply.github.com>
2022-04-13 16:23:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var audio= document.createElement('audio');
+audio.setAttribute('src','sound.mp3');
+audio.setAttribute('autoplay','sound.mp3');
+audio.loop=true;
+audio.play();
+
+// var sound = new Howl({
+//     src: ['sound.mp3'],
+//     autoplay: true,
+//     loop: true,
+//     volume: 0.5,
+// });
+
+// sound.play();
